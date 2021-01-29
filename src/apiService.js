6431f852cf6858f360c7b8bc6ed0a82d51f4ec10
Oksana Yurchenkovych <oksanayurchenkovych@gmail.com>
2021-01-29 15:52:21 +0200

    base

---
+++ src/apiService.js
@@ -0,0 +1,5 @@
+const key = '20073570-94be55d6c10f1ac23fcd8c784';
+const baseUrl = 'https://pixabay.com/api/';
+
+let params =
+  '?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key';
